897e1649e0322d0ea85ada52b4f979e3d9b4e688
will <wylie1027@comcast.net>
2021-02-23 18:07:05 -0500

    Added Minesweeper page

---
+++ GridGames/ClientApp/src/app/minesweeper/minesweeper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minesweeper-component',
+  templateUrl: './minesweeper.component.html'
+})
+export class MinesweeperComponent{
+    
+
+}
