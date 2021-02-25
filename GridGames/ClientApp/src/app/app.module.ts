811f422d796ba4473de3b33f150c2b0673db7f2a
Stanton Schmitz <strain2900@gmail.com>
2021-02-25 13:44:40 -0800

    Attempted to make new branch for Sudoku dev Attempted to make a Sudoku folder in a similar fasion to Minesweeper folder; not sure if it worked.

--- GridGames/ClientApp/src/app/app.module.ts
+++ GridGames/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MinesweeperComponent } from './minesweeper/minesweeper.component';
+import { SudokuComponent } from './sudoku/sudoku.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MinesweeperComponent } from './minesweeper/minesweeper.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    MinesweeperComponent
+    MinesweeperComponent,
+    SudokuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +30,8 @@ import { MinesweeperComponent } from './minesweeper/minesweeper.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'minesweeper', component: MinesweeperComponent }
+      { path: 'minesweeper', component: MinesweeperComponent},
+      { path: 'sudoku', component: SudokuComponent }
     ])
   ],
   providers: [],
