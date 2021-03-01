d9f1e3e62fc3ffe97cd2f4d0c5e56fc5f4d6c8d7
Will Holland <wylie1027@comcast.net>
2021-03-01 16:42:45 -0500

    Added routing for the paths to component.html files for nonogram and sudoku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MinesweeperComponent } from './minesweeper/minesweeper.component'
+import { NonogramComponent } from './nonogram/nonogram.component'
+import { SudokuComponent } from './sudoku/sudoku.component'
 
 const routes: Routes = [
-	{path: 'minesweeper', component: MinesweeperComponent}
+	{path: 'minesweeper', component: MinesweeperComponent}, 
+	{path: 'nonogram', component: NonogramComponent}, 
+	{path: 'sudoku', component: SudokuComponent}
 
 ];
 
