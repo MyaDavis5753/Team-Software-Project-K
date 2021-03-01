157116c4e2b00a4b20487db1d3787c30447cde3f
Will Holland <wylie1027@comcast.net>
2021-03-01 16:36:20 -0500

    Fixed the new components so they weren't within the folder of the minesweeper component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MinesweeperComponent } from './minesweeper/minesweeper.component';
-import { NonogramComponent } from './minesweeper/nonogram/nonogram.component';
-import { SudokuComponent } from './minesweeper/sudoku/sudoku.component';
+import { SudokuComponent } from './sudoku/sudoku.component';
+import { NonogramComponent } from './nonogram/nonogram.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MinesweeperComponent,
-    NonogramComponent,
-    SudokuComponent
+    SudokuComponent,
+    NonogramComponent
   ],
   imports: [
     BrowserModule,
